ee836081044481fd7b202d4ba65e53eef0ca1fa8
Harshita Sinari <harshitasinari@mbk-tech-526-ub.mobikwik.com>
2020-11-28 19:46:06 +0530

    Random Fortune Service added

---
+++ src/com/harshita/spring/RandomFortuneService.java
@@ -0,0 +1,12 @@
+package com.harshita.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService{
+
+    @Override
+    public String getFortune() {
+        return "This is a Random Fortune Service";
+    }
+}
--- src/com/harshita/spring/TennisCoach.java
+++ src/com/harshita/spring/TennisCoach.java
@@ -1,13 +1,15 @@
 package com.harshita.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
     @Autowired
-    FortuneService fortuneService;
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
     @Override
     public String getDailyWorkout() {
         return "Hey Play Tennis Today";
